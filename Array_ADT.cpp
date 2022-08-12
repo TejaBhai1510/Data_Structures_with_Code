7d1df3c1adf9231a3e0ee2a8e73626c6929ccedc
Tejas Bhandvalkar <90249486+TejaBhai1510@users.noreply.github.com>
2022-08-12 15:44:07 +0530

    Add files via upload

---
+++ Array_ADT.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Array{
+
+private:
+    int *A;
+    int size;
+    int length;
+
+public:
+    Array(int size){
+        this->size = size;
+        A = new int[size];
+    }
+
+    void create(){
+        cout << "Enter number of elements: " << flush;
+        cin >> length;
+        cout << "Enter the array elements: " << endl;
+        for (int i = 0; i < length; i++){
+            cout << "Array element " << i+1 << " = " << flush;
+            cin >> A[i];
+        }
+    }
+
+    void display(){
+        for (int i = 0; i < length; i++){
+            cout << A[i] << " " << endl;
+        }
+    }
+
+    ~Array(){
+        delete[] A;
+        cout << "Array destroyed" << endl;
+    }
+};
+
+int main(){
+    Array arr(10);
+    arr.create();
+    arr.display();
+    return 0;
+}
